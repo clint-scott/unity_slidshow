53aa4342f0af32912f6bc47512e7d55a194ce36a
Clint Scott <cscott@cgsinc.ca>
2019-09-05 21:55:05 -0300

    Res & Sequence change

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -7,117 +7,159 @@ using DentedPixel;
 
 public class UIManager : MonoBehaviour
 {
-	public GameObject Canvas,MainMenu,BtnWrapper;
+	public GameObject Canvas,MainMenu,BackgroundImage;
 	public float TweenDuration = 0.5f;
-	public int ButtonWidth = 512;
+	public float ButtonWidth = 45;
 	public VideoClip[] VideoClips;
-	public List<GameObject> Buttons = new List<GameObject>();
+	public List<GameObject> MenuButtons = new List<GameObject>();
 
 
 
-	private List<GameObject> menus = new List<GameObject>();
+	private List<GameObject> screens = new List<GameObject>();
 	private VideoPlayer videoPlayer;
 	private AudioSource audioSource;
 	private Vector3[] screenVectors;
-	private int menuIndex = 1;
-	private float delay = 0,canvasWidth,canvasHeight,btnWrapperVectorX;
+	private int screenIndex = 1;
+	private bool pauseVideo = true;
+	private float canvasWidth,canvasHeight,btnWrapperVectorX;
+	private DeviceOrientation orientation;
 	private Coroutine coroutine;
 
 
-
 	public void Start () {
-		canvasWidth = Canvas.GetComponent<CanvasScaler>().referenceResolution.x;
-		canvasHeight = Canvas.GetComponent<CanvasScaler>().referenceResolution.y;
-
-		screenVectors = new Vector3[] {
-			new Vector3(-canvasWidth,0,0),
-			new Vector3(0,canvasHeight,0),
-			new Vector3(canvasWidth,0,0),
-			new Vector3(0,-canvasHeight,0),
-			new Vector3(-canvasWidth,0,0),
-			new Vector3(0,canvasHeight,0),
-		};
-
-		btnWrapperVectorX = (canvasWidth/2)+ButtonWidth;
+		orientation = Input.deviceOrientation;
+		
+		updateOrientation();
 
 		int index = 0;
-
-		foreach(GameObject button in Buttons) {
+		foreach(GameObject button in MenuButtons) {
 			float xPos = Mathf.Round((ButtonWidth*1.5f)*index);
+			RectTransform btnRect = button.GetComponent<RectTransform>();
+			btnRect.sizeDelta = new Vector2(ButtonWidth, ButtonWidth);
 
 			button.transform.localPosition = new Vector3(xPos,0,0);
 			index++;
 		}
+	
+		LeanTween.scale(BackgroundImage.GetComponent<RectTransform>(),new Vector2(1.2f,1.2f),30f)
+			.setLoopPingPong ()
+			.setEaseInOutQuint();
 
-		Invoke("selectScreen",2f);
+		//Debug.Log("***** canvasResX: "+canvasResX+" | canvasResY: "+canvasResY+" | canvasWidth: "+canvasWidth);
+		//Debug.Log("***** screenWidth: "+Screen.width+" | screenHeight: "+Screen.height);
+
+		Invoke("screenSelect",2f);
 	}
 
-	public void selectScreen() {
+	public void Update() {
+		if(orientation != Input.deviceOrientation) {
+			updateOrientation();
+		}
+	}
 
-		if(menus.Count > 0) {
-			closeScreen();
-			//delay = menus.Count > 0 ? TweenDuration : 0;
+	private void updateOrientation() {
+		//Debug.Log("**** updateOrientation ****");
+
+		bool isLandscape = Screen.width > Screen.height;
+		float canvasResX = Canvas.GetComponent<CanvasScaler>().referenceResolution.x;
+		float canvasResY = Canvas.GetComponent<CanvasScaler>().referenceResolution.y;
+
+		canvasWidth = canvasResX+ButtonWidth;
+		canvasHeight = canvasResY+ButtonWidth;
+
+		ButtonWidth = Mathf.Round(canvasResX*(ButtonWidth/100));
+
+		//float stageWidth = isLandscape ? canvasWidth : canvasHeight;
+		float stageHeight = isLandscape ? canvasHeight : canvasWidth;
+
+		float modCanvasHeight = canvasWidth*(16/9);
+
+		screenVectors = new Vector3[] {
+			new Vector3(-canvasWidth,0,0),
+			new Vector3(0,stageHeight,0),
+			new Vector3(canvasWidth,0,0),
+			new Vector3(0,-stageHeight,0),
+			new Vector3(-canvasWidth,0,0),
+			new Vector3(0,stageHeight,0),
+		};
+
+		btnWrapperVectorX = (canvasWidth/2)+ButtonWidth;
+		MainMenu.transform.localPosition = new Vector3(btnWrapperVectorX,0,0);
+	}
+
+	public void screenSelect() {
+
+		if(screens.Count > 0) {
+			screenClose();
+			//delay = screens.Count > 0 ? TweenDuration : 0;
 		}
 
-		delay = menuIndex == 1 ? 3f : 0;
+		//delay = screenIndex == 1 ? 3f : 0;
 
-		GameObject menu = new GameObject("Menu_0"+menuIndex,typeof(RectTransform));
-	
-		RawImage rawImage = menu.AddComponent<RawImage>();
+		GameObject screen = new GameObject("Screen_0"+screenIndex,typeof(RectTransform));
+		AspectRatioFitter ratioFitter = screen.AddComponent<AspectRatioFitter>();
+
+		RawImage rawImage = screen.AddComponent<RawImage>();
 			rawImage.color = new Color(255,255,255);
 		
-		RectTransform rectTransform = menu.GetComponent<RectTransform>();
+		RectTransform rectTransform = screen.GetComponent<RectTransform>();
 		
 		rectTransform.sizeDelta = Vector2.zero;
         rectTransform.anchorMin = Vector2.zero;
         rectTransform.anchorMax = Vector2.one;
         rectTransform.anchoredPosition = new Vector2(.5f,.5f);
 
-		menu.transform.localPosition = screenVectors[menuIndex-1];
-		menu.transform.SetParent(Canvas.transform,false);
-		menus.Add(menu);
+		ratioFitter.aspectMode = AspectRatioFitter.AspectMode.WidthControlsHeight;
+		ratioFitter.aspectRatio = 1.777f;
 
+		screen.transform.localPosition = screenVectors[screenIndex-1];
+		screen.transform.SetParent(Canvas.transform,false);
+		screens.Add(screen);
 
-		Invoke("animateButtonWrapper",delay);
+		coroutine = StartCoroutine(initVideo());
+		//Invoke("animateMainMenu",delay);
 		
 		//Invoke("playVideo",delay);
 	}
 
 	private void animateButton(float duration) {
-		
-		float shortTween = TweenDuration*.25f;
+		float shortTween = TweenDuration*.5f;
 		float longTween = TweenDuration*2f;
-		float delay = menuIndex == 1 ? duration-longTween : 0;
+		float delay = screenIndex == 1 ? Mathf.Max(duration-longTween, 0) : 0;
 
-		if(menuIndex>1) {
-			LeanTween.scale( Buttons[menuIndex-2].GetComponent<RectTransform>(),new Vector3(1f,1f,1f),longTween).setEaseInOutQuad();
+		if(screenIndex>1) {
+			RectTransform oldBtnRect = MenuButtons[screenIndex-2].GetComponent<RectTransform>();
+			LeanTween.scale(oldBtnRect,new Vector3(1f,1f,1f),longTween)
+				.setEaseInOutQuad();
 		};
 		
+		RectTransform newBtnRect = MenuButtons[screenIndex-1].GetComponent<RectTransform>();
 		
 		LTSeq seq = LeanTween.sequence();
 			seq.append(delay);
 			seq.append(
-				LeanTween.scale( Buttons[menuIndex-1].GetComponent<RectTransform>(),new Vector3(1.3f,1.3f,1f),longTween)
-					.setEaseInOutQuad()
-					.setDelay(delay)
+				LeanTween.scale(newBtnRect,new Vector3(1.4f,1.4f,1f),longTween)
+						.setEaseInOutQuad()
+						.setDelay(delay)
 			);
 			seq.append(0.75f);
 			seq.append(
-				LeanTween.scale( Buttons[menuIndex-1].GetComponent<RectTransform>(),new Vector3(1.2f,1.2f,1f),shortTween)
+				LeanTween.scale(newBtnRect,new Vector3(1.3f,1.3f,1f),shortTween)
 					.setEaseInOutQuad()
 					.setLoopPingPong (1)
 					.setOnComplete(() => {
 						//Invoke("playVideo",0.25f);
-						coroutine = StartCoroutine(initVideo());
+						//coroutine = StartCoroutine(initVideo());
+						Invoke("screenOpen",0.5f);
 					})
 			);
 	}
 
-	private void animateButtonWrapper() {
+	private void animateMainMenu() {
 		
 		float offset = Mathf.Round(ButtonWidth*1.5f);
 
-		float newVectorX = 0-((menuIndex-1)*offset);
+		float newVectorX = 0-((screenIndex-1)*offset);
 		float distance = btnWrapperVectorX - newVectorX;
 
 		btnWrapperVectorX = newVectorX;
@@ -126,42 +168,46 @@ public class UIManager : MonoBehaviour
 
 		float duration = getTransitionDuration(distance);
 
-		LeanTween.move( BtnWrapper.GetComponent<RectTransform>(),btnWrapperVector,duration).setEaseInOutQuad();
+		LeanTween.move(MainMenu.GetComponent<RectTransform>(),btnWrapperVector,duration).setEaseInOutQuad();
 		
 		animateButton(duration);
 	}
 
-    private void openScreen() {
-		LeanTween.move( menus[menus.Count-1].GetComponent<RectTransform>(),Vector3.zero,TweenDuration).setEaseInOutQuad();
+    private void screenOpen() {
+		LeanTween.move(screens[screens.Count-1].GetComponent<RectTransform>(),Vector3.zero,TweenDuration).setEaseInOutQuad().setOnComplete(() => {
+			pauseVideo = false;
+		});
 	}
 
-	public void closeScreen() {
-		LeanTween.move( menus[menus.Count-1].GetComponent<RectTransform>(),screenVectors[menuIndex-1],TweenDuration)
+	public void screenClose() {
+		LeanTween.move(screens[screens.Count-1].GetComponent<RectTransform>(),screenVectors[screenIndex-1],TweenDuration)
 		.setEaseInQuad()
 		.setOnComplete(() => {
-			removePanel();
-			menuIndex++;
-			Debug.Log("***** menuIndex: "+menuIndex+" | VideoClips.Length: "+VideoClips.Length);
-			if(menuIndex-1 < VideoClips.Length) {
-				selectScreen();
+			screenRemove();
+			screenIndex++;
+			
+			if(screenIndex-1 < VideoClips.Length) {
+				screenSelect();
 			}
 		});
 	}
 
-	private void removePanel() {
-		Destroy(menus[0]);
-		menus.RemoveAt(0);
+	private void screenRemove() {
+		Destroy(screens[0]);
+		screens.RemoveAt(0);
 	}
 
 	IEnumerator initVideo() {
-		//Debug.Log("********** Initializing Video");
+		//Debug.Log("********** Initializing Video | delay: "+delay);
+
 		videoPlayer = gameObject.AddComponent<VideoPlayer>();
 		audioSource = gameObject.AddComponent<AudioSource>();
 
-		GameObject menu = menus[menus.Count-1];
-		RawImage rawImage = menu.GetComponent<RawImage>();
+		GameObject screen = screens[screens.Count-1];
+		RawImage rawImage = screen.GetComponent<RawImage>();
 		
 		videoPlayer.playOnAwake = false;
+		videoPlayer.waitForFirstFrame = true;
 		audioSource.playOnAwake = false;
 		audioSource.Pause();
 		
@@ -172,7 +218,7 @@ public class UIManager : MonoBehaviour
 		videoPlayer.EnableAudioTrack(0,true);
 		videoPlayer.SetTargetAudioSource(0,audioSource);
     
-		videoPlayer.clip = VideoClips[menuIndex-1];
+		videoPlayer.clip = VideoClips[screenIndex-1];
 		videoPlayer.Prepare();
 
 		WaitForSeconds waitTime = new WaitForSeconds (1);
@@ -184,27 +230,41 @@ public class UIManager : MonoBehaviour
 			break;
 		}
 
+		
 		//Debug.Log("********** Done Preparing Video");
 
 		rawImage.texture = videoPlayer.texture;
 		
 		videoPlayer.Play();
 		audioSource.Play();
-		
-		openScreen();
-		
-		//Debug.Log("********** Playing Video");
-        while (videoPlayer.isPlaying)
+
+		while (videoPlayer.frame  < 2) {
+			yield return null;
+		}
+
+		videoPlayer.Pause();
+		animateMainMenu();
+
+		while (pauseVideo) {
+			yield return null;
+		}
+
+		videoPlayer.Play();
+		audioSource.Play();
+
+    	while (videoPlayer.isPlaying)
         {
             ////Debug.LogWarning("Video Time: " + Mathf.FloorToInt((float)videoPlayer.time));
             yield return null;
         }
-        Debug.Log("********** Done Playing Video");
+		
+		pauseVideo = true;
 		StopCoroutine(coroutine);
-		closeScreen();
+		screenClose();
 	}
 
 	private float getTransitionDuration(float distance) {
+		Debug.Log("***** getTransitionDuration | distance: "+distance+" | ButtonWidth: "+ButtonWidth);
 		return (distance/(ButtonWidth))*TweenDuration;
 	}
     
